ffe5055e06208364593321406e785a43ac02a7fa
jcgrubert <juho.grubert@gmail.com>
2021-03-08 23:03:17 +0200

    added backend, to start it call npm start in root folder

--- partyguru_singlepage/src/screens/HomeScreen.js
+++ partyguru_singlepage/src/screens/HomeScreen.js
@@ -1,12 +1,13 @@
 
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Product from '../components/Product';
-import data from '../data';
+//import data from '../data';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 //splash sivu
 //map funktio looppaa datan läpi ja tekee niistä kortteja
 //Product.js tekee ne kortit viime kädessä
@@ -21,54 +22,97 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.secondary,
   },
 }));
-/* function CenteredGrid(props) {
-  const classes=useStyles();
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={3} justify="center">
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12, Ilmeisesti gridin rivillä on aina 12 yksikköä tilaa</Paper>
-        </Grid>
-        <Grid item xs={1}>
-          <Paper className={classes.paper}>xs=1</Paper>
-        </Grid>
-        <Grid item xs={1}>
-          <Paper className={classes.paper}>xs=1</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={1}>
-          <Paper className={classes.paper}>xs=1</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-      </Grid>
-    </div>
-  );
-} */
+
 const HomeScreen = () => {
+
+  const [products, setProduct] = useState([]);
+  const [singleProduct, setSingleProduct] = useState([]);
+  const [singleid, setSingleid]=useState(1);
+  
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      axios.get('/api/products').then(response => {
+      const {data} = response.data;
+      setProduct(response.data);
+      console.log(data)
+})
+    }
+    fetchData();
+    return () => {
+      //
+    };
+  }, []);
+  
+  //Kun products muuttuu, alustetaan singleID
+  useEffect(() => {
+    let found=products.filter(item => {
+      return item._id === singleid
+    })
+    for (var i = 0; i < products.length; i++){
+      if (products[i]._id == singleid){
+         found=products[i];
+         setSingleProduct(found);
+      }
+    }
+    return () => {
+      //
+    };
+  }, [products]);
+
+//Kun singleid muuttuu, etsi sen arvon mukainen partypackagedata ja laita se singleProductiin
+  useEffect(() => {
+    let found=products.filter(item => {
+      return item._id === singleid
+    })
+    let foundBoolean=false;
+    for (var i = 0; i < products.length; i++){
+      if (products[i]._id == singleid){
+         foundBoolean=true;
+         found=products[i];
+         setSingleProduct(found);
+      }
+    }
+    if(foundBoolean===false){
+      setSingleid(1);
+    }
+    return () => {
+      //
+    };
+  }, [singleid]);
+
+  //Nappi mikä vaihtaa partypackagekorttia, sen klikinhändlääjä
+  //klikkauksessa vaan kasvatetaan singleid:tä, mikä triggeröi ylläolevan hookin^
+  const handleClick = () => {
+    let __sid=singleid;
+    __sid+=1;
+    setSingleid(__sid);
+   }
+   
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <Grid container spacing={4} justify="center">
       
-        {data.products.map((product) => (
+        {products.map((product) => (
           <Product key={product._id} product={product}></Product>
         ))}
-      
       </Grid>
       <div className="row">uusi rivi</div>
-      <Button variant="contained" color="primary">
-      Hello Button
+      <Button variant="contained" color="primary" onClick={() => { handleClick() }}>
+      Next package button
     </Button>
         <Link to='/gurupage/'>Click here if you are the party guru</Link>
 
-
+        <Grid container spacing={4} justify="center">
+        
+        {/* tämän kortin sisältö vaihtuu napista */}
+       <Product key={singleid} product={singleProduct}></Product>
+        
+      
+      
+    
+    </Grid>
     </div>
   );
 }
