2bf5e39b881e262f209df47351c470c6224dd359
juvalta <62938153+juvalta@users.noreply.github.com>
2021-03-10 08:46:07 +0200

    Added Party Page Creation page elements

--- partyguru_singlepage/src/screens/CreatePartyPage.js
+++ partyguru_singlepage/src/screens/CreatePartyPage.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import Product from '../components/Product';
-import data from '../data';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 import {Link} from "react-router-dom";
+import "./CreatePartyPage.css"
 
 const CreatePartyPage = () => {
     return(
         <div>
-            <p>Here be Party Page Creation page</p>
-             <Link to ="/invitetoparty">To invitation creation</Link> 
-        </div>
+          <div className ="partyguruinfobox">
+            <h1>Party Guru</h1>
+            <p>
+                Our Party Gurus host the most awesome parties for partiers of any age.<br/>
+                Parties are hosted on the Discord voice application and in various games <br/>
+                available in our selection. <br/>
+                Choose your package and get partying. Do it now!
+            </p>
+          </div>
+          
+          <div>Schedule</div>
+          <div class="schedulebox">
+            <div>Minecraft</div>
+            <div>Birthday Cheer</div>
+            <div>Minecraft mod 1</div>
+            <div>Eat cake</div>
+            <div>Minecraft mod 2</div>
+          </div>
+          <div>Available activities</div>
+          <div class="schedulebox">
+            <div>Minecraft</div>
+            <div>Birthday Cheer</div>
+            <div>Minecraft mod 1</div>
+            <div>Eat cake</div>
+            <div>Minecraft mod 2</div>
+          </div>
+          <div>
+          <Link to ="/invitetoparty">To invitation creation</Link> 
+          </div>
+          </div>
     )
 }
 
