a8a65099277ea34bef0b778f6f1dbef2d5b475dd
pklemettila <pauli.klemettila@gmail.com>
2021-03-11 20:04:53 +0200

    you can now edit your guru profile (locally)

--- partyguru_singlepage/src/screens/GuruPage/components/GuruInfo.js
+++ partyguru_singlepage/src/screens/GuruPage/components/GuruInfo.js
@@ -1,29 +1,86 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import {TextField} from "@material-ui/core";
 
 
 const GuruInfo = (props) =>{
 
+
     const [editMode, setEditMode] = useState(false)
-    const [bio, setBio] = useState("Lorem ipsum dolor sit amet...")
+    const [guruInfo, setGuruInfo] = useState({
+        name: "", nick: "", bio: ""})
+    const [editedGuruInfo, setEditedGuruInfo] = useState({
+        name: "", nick: "", bio: ""})
+
+
+    // Initially setting the state of the guruInfo object on render
+    useEffect(() => {
+        const newGuruInfo = {name: props.name, nick:props.nick, bio:props.bio}
+        setGuruInfo(newGuruInfo);
+        setEditedGuruInfo(newGuruInfo)
+    }, [props]);
+
 
     const handleClick = () => {
+        setEditMode(!editMode)
+    }
+
+
+    const handleNameChange = (event) => {
+        const newGuruInfo= {
+            ...editedGuruInfo, name: event.target.value
+        }
+        setEditedGuruInfo(newGuruInfo)
+    }
+
+    const handleNickChange = (event) => {
+        const newGuruInfo= {
+            ...editedGuruInfo, nick: event.target.value
+        }
+        setEditedGuruInfo(newGuruInfo)
+    }
+
+    const handleBioChange = (event) => {
+        const newGuruInfo= {
+            ...editedGuruInfo, bio: event.target.value
+        }
+        setEditedGuruInfo(newGuruInfo)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setGuruInfo(editedGuruInfo)
+        setEditedGuruInfo(guruInfo)
+        setEditMode(!editMode)
 
     }
 
 
-
-    return (
+    if (editMode === false) return (
         <div className="info">
             <p>
                 <b>Guru Info:</b><br/>
-                Name: {props.name} <br/>
-                Nick: {props.nick}<br/>
+                Name: {guruInfo.name} <br/>
+                Nick: {guruInfo.nick}<br/>
                 BIO: <br/>
-                {props.bio} <br/>
+                {guruInfo.bio} <br/>
                 <button onClick={handleClick}>Edit</button>
 
             </p>
         </div>
+    )
+
+    if (editMode === true) return (
+
+        <div>
+            <form onSubmit={handleSubmit}>
+            <TextField value={editedGuruInfo.name} label="name" onChange={handleNameChange}/> <br/>
+            <TextField value={editedGuruInfo.nick} label="nick" onChange={handleNickChange}/> <br/>
+            <TextField value={editedGuruInfo.bio} multiline label="bio" onChange={handleBioChange}/>  <br/>
+            <button type="submit">Save</button>
+            </form>
+
+        </div>
+
     )
 
 }
