1f564ecdb0c721df48579df7cef99ea85a8a1152
juvalta <62938153+juvalta@users.noreply.github.com>
2021-03-17 14:00:53 +0200

    Merge error commit

--- partyguru_singlepage/src/screens/InviteToParty.js
+++ partyguru_singlepage/src/screens/InviteToParty.js
@@ -1,13 +1,37 @@
-import react from "react"
+import react, { useState } from "react"
 import {Link} from "react-router-dom"
+import "./InviteToParty.css"
+
+
+const handleSubmit = () => {
+    return(0)
+}
+
+const handleChange = (event) => {
+
+}
 
 const InviteToParty = () => {
     
+    const[description, changeDescription] = useState("Please input a party description that will be included in the party invitations by email.");
+
     return(
     <div>
         <Link to="/createpartypage">Back</Link>
-        <h1>Party Package finalization 2/2</h1>
-        <p>Personalize party invitations</p>
+        <div class="mainheader">
+            <h1>Party Package finalization 2/2</h1>
+            <p>Personalize party invitations</p>
+        </div>
+        
+        
+        <div class="partydescription">
+            <h1>Party description</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <input type="text" value={description}/>
+                </label>
+            </form>
+        </div>
         <Link to="/partypage">Send Invites and go to party page</Link>
     </div>
     )
