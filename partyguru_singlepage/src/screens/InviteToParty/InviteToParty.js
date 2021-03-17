8161a751351a2a6d260a78908440a3da077c68b1
juvalta <62938153+juvalta@users.noreply.github.com>
2021-03-17 15:41:18 +0200

    InviteToParty broken a bit

--- partyguru_singlepage/src/screens/InviteToParty/InviteToParty.js
+++ partyguru_singlepage/src/screens/InviteToParty/InviteToParty.js
@@ -1,19 +1,49 @@
-import react, { useState } from "react"
+import react, { useState, useEffect } from "react"
 import {Link} from "react-router-dom"
 import "./InviteToParty.css"
+import axios from 'axios'
+import { response } from "express"
 
 
-const handleSubmit = () => {
-    return(0)
-}
 
-const handleChange = (event) => {
 
-}
 
 const InviteToParty = () => {
     
+    //VAIHTAA DESCRIPTION-KENTÄN TILAA. TÄHÄN KIRJOITETAAN EMAILIN SISÄLTÖ
     const[description, changeDescription] = useState("Please input a party description that will be included in the party invitations by email.");
+
+    useEffect(() => {
+        //KAATAA KOODIN
+        const partypack = getData()
+        console.log(partypack, ' was passed from getData')
+        //TARKOITUS LAITTAA DESCRIPTION TEKSTIKENTÄN TEKSTIKSI ALUSSA
+        //changeDescription(partypack.description)
+      }, [])
+    
+    const getData = () => {
+        let id=1;
+        axios.get(`/api/parties/${id}`).then(response => {
+        
+        //setState(response.data);
+        const {partypack} = response.data
+        
+        
+        
+        changeDescription(response.data.description)
+        console.log(response.data);
+        //return(partypack)
+        //return(response.data)
+    })
+    }
+
+    const handleChange = (event) => {
+        changeDescription(event.target.value)
+    }
+
+    const handleSubmit = () => {
+        return(0)
+    }
 
     return(
     <div>
@@ -28,7 +58,7 @@ const InviteToParty = () => {
             <h1>Party description</h1>
             <form onSubmit={handleSubmit}>
                 <label>
-                    <input type="text" value={description}/>
+                    <input type="text" /*value={description}*/ onChange={handleChange}/>
                 </label>
             </form>
         </div>
