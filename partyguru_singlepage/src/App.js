fc92b3d76ebd1e99acc8e01cf6366a90c2ac672e
Joonas <joonas.vuolukka@pp.inet.fi>
2021-03-17 10:42:32 +0200

    Project directory restructured, edited import addresses to match

--- partyguru_singlepage/src/App.js
+++ partyguru_singlepage/src/App.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import LandingPage from "./screens/LandingPage";
-import PartyPackage from "./screens/PartyPackage";
-import GuruPage from "./screens/GuruPage/GuruPage";
-import CreatePartyPage from "./screens/CreatePartyPage"
-import CartPage from "./screens/CartPage";
-import LoginPage from './screens/LoginPage';
-import InviteToParty from "./screens/InviteToParty"
-import PartyPage from "./screens/PartyPage";
+
+//Import Material-ui components here
 import { colors } from '@material-ui/core';
-import LoginPage from './screens/LoginPage';
-import CartPage from './screens/CartPage';
+
+//Import pages here
+import LandingPage from "./screens/LandingPage/LandingPage";
+import PartyPackage from "./screens/PartyPackage/PartyPackage";
+import GuruPage from "./screens/GuruPage/GuruPage";
+import CreatePartyPage from "./screens/CreatePartyPage/CreatePartyPage"
+import CartPage from "./screens/CartPage/CartPage";
+import LoginPage from './screens/LoginPage/LoginPage';
+import InviteToParty from "./screens/InviteToParty/InviteToParty"
+import PartyPage from "./screens/PartyPage/PartyPage";
+
 //Browserrouter hoitaa tän urlien reitityksen
 //Kaikki HTML koodi mitä tässä näkyy ajetaan joka sivulla,
 //paitsi nuo <Route> tagit, jotka ajetaan vain URLin mukaisella sivulle
--- partyguru_singlepage/src/screens/PartyPackage/PartyPackage.js
+++ partyguru_singlepage/src/screens/PartyPackage/PartyPackage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import data from '../data';
+import data from '../../data';
 //package kortit linkkaa tänne
 //jätin tän vaan siksi että näkyisi miten eri screenejä voi tehä browserroutella, oikeasti varmaan meillä ei oo mitään tällaista screeniä
 const PartyPackage = (props)=> {
