9f8e31a30329565dddccaafafe3a579351bd6175
pklemettila <pauli.klemettila@gmail.com>
2021-03-17 20:09:43 +0200

    Worked on state management for checkboxes

--- partyguru_singlepage/src/screens/GuruPage/components/GuruPartyPackages.js
+++ partyguru_singlepage/src/screens/GuruPage/components/GuruPartyPackages.js
@@ -4,30 +4,54 @@ import { Checkbox } from '@material-ui/core';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 
-const GuruPartyPackages = (props) =>{
+const GuruPartyPackages = ({guruID}) =>{
 
 
 
     const [pPackages, setPPackages] = useState([])
+
+    //array used to control the checked status of dynamically created checkboxes
     const [checked, setChecked] = useState([])
+    const [checkEditMode, setCheckEditMode] = useState(true)
 
 
     useEffect(() => {
+
         axios.get('/api/packages').then(response => {
             setPPackages(response.data)
+
+            const newCheckedArray = []
+            response.data.map(pPackage => newCheckedArray.push(pPackage.guru.includes(guruID))
+            )
+            console.log(newCheckedArray)
+            setChecked(newCheckedArray)
+
         })
+        
 
-    }, []);
-
-    const updateGuruPackages = (event) => {
+    }, [guruID]);
 
 
+    const handleChange = (id) => {
+
+        const newCheckedArray = (checked.concat())
+        newCheckedArray[id] = !checked[id]
+        setChecked(newCheckedArray)
     }
 
+    const handleClick =() =>{
+        setCheckEditMode(!checkEditMode)
+    }
+
+
     return (
+
         <div>
             {pPackages.map(pPackage =>
-                <FormControlLabel key={pPackage._id}  control={<Checkbox name={pPackage.name} checked={pPackage.guru.includes(props.guruID)}/>} label={pPackage.name} /> )}
+                <FormControlLabel key={pPackage._id}  control=
+                    {<Checkbox onChange={() => handleChange(pPackage._id-1)} name={pPackage.name}
+                               checked={checked[pPackage._id-1] || false}/>} label={pPackage.name} disabled={checkEditMode}/> )}
+                               <button onClick={handleClick}>Edit</button>
         </div>
     )
 
