bc6c69540df3cf9e4062ff4bd565c2d17ec81783
pklemettila <pauli.klemettila@gmail.com>
2021-03-18 22:08:29 +0200

    changed strings to ints in partypack.js guru arrays

--- partyguru_singlepage/src/screens/GuruPage/components/GuruImage.js
+++ partyguru_singlepage/src/screens/GuruPage/components/GuruImage.js
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
+import {TextField} from "@material-ui/core";
+import {Dialog} from "@material-ui/core";
+import {DialogTitle} from "@material-ui/core";
+
 
 const GuruImage = (props) =>{
 
-    return (
+
+
+
+    const [guruImage, setGuruImage] = useState("")
+    const [newGuruImageAddress, setNewGuruImageAddress] = useState("")
+    const [dialogOpen, setDialogOpen] = useState(false)
+
+
+    useEffect(() => {
+        setGuruImage(props.guruImage)
+        setNewGuruImageAddress(props.guruImage)
+    }, [props]);
+
+    const handleClick = () => {
+        setDialogOpen(true)
+    }
+
+
+    const handleClose = () => {
+        setDialogOpen(false)
+    }
+
+
+    const handleChange = (event) => {
+        setNewGuruImageAddress(event.target.value)
+    }
+
+   return (
         <div>
-            <img className="profilePic" src={props.guruImage} alt={"will smith"} width={300} height={300}   />
-            <button>Change Pic</button>
+            <img className="profilePic" src={guruImage} alt={"will smith"} width={300} height={300}   />
+            <button onClick={handleClick}>Change Pic</button>
+            <Dialog open={dialogOpen} onClose={handleClose}>
+            <DialogTitle>Get rekt</DialogTitle>
+            </Dialog>
         </div>
     )
+
+
+
 
 }
 
