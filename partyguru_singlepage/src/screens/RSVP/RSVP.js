6830c515e8c8cbbc33c8c90ba9e3b3bbc82fe65e
Joonas <joonas.vuolukka@pp.inet.fi>
2021-03-29 18:04:19 +0300

    RSVP reworked with Material UI

--- partyguru_singlepage/src/screens/RSVP/RSVP.js
+++ partyguru_singlepage/src/screens/RSVP/RSVP.js
@@ -1,62 +1,76 @@
-import React from 'react'
-import {useState} from 'react'
-import {makeStyles, Button, Checkbox, FormControlLabel} from '@material-ui/core'
-import createSpacing from '@material-ui/core/styles/createSpacing'
-import "./RSVP.css"
+import React from "react";
+import { useState } from "react";
+import {
+  makeStyles,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+  Paper,
+  Grid,
+} from "@material-ui/core";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
+import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
+import createSpacing from "@material-ui/core/styles/createSpacing";
+import "./RSVP.css";
 
-const useStyles= makeStyles((theme) => ({
-    buttons: {
-        //padding: '10px 10px',
-        //paddingTop: '10px',
-        //paddingBottom: '10px',
-        //padding: theme.spacing(3),
-        margin: '10px',
-    },
-    checkBox: {
-        fontSize: '3rem',
-    }
-}))
+const useStyles = makeStyles((theme) => ({
+  buttons: {
+    margin: "10px",
+  },
+  checkBoxFont: {
+    fontSize: "2rem",
+  },
+  checkBoxPaper: {
+    padding: "5px",
+    margin: "5px",
+    width: "20vw",
+    elevation: "3",
+  },
+}));
 
 const RSVP = () => {
+  const [checked, setChecked] = useState({
+    attending: false,
+    notattending: false,
+  });
+  const [gamingspecs, setSpecs] = useState({
+    gameinstalled: false,
+    gamenotinstalled: false,
+    discordinstalled: false,
+    discordnotinstalled: false,
+  });
+  //const [attending, changeAttending] = useState(true)
+  //const [notattending, changeNotAttending] = useState(false)
 
-    const [checked, setChecked] = useState({
-        attending: false,
-        notattending: false,
-    })
-    const [gamingspecs, setSpecs] = useState({
-        gameinstalled: false,
-        gamenotinstalled: false,
-        discordinstalled: false,
-        discordnotinstalled: false,
-    })
-    //const [attending, changeAttending] = useState(true)
-    //const [notattending, changeNotAttending] = useState(false)
+  //äääääääää
 
-    //äääääääää
+  const handleGamingSpecChange = (event) => {
+    console.log(
+      event.target.name,
+      " has changed its state to",
+      event.target.checked
+    );
+    setSpecs({ ...gamingspecs, [event.target.name]: event.target.checked });
+  };
 
-    const handleGamingSpecChange= (event) => {
-        console.log(event.target.name, " has changed its state to", event.target.checked)
-        setSpecs({ ...gamingspecs, [event.target.name]: event.target.checked})
-    }
+  const handleChange = (event) => {
+    console.log(event.target.name);
+    //setChecked(event.target.checked)
+    setChecked({ ...!checked, [event.target.name]: event.target.checked });
+  };
 
-    const handleChange = (event) => {
-        console.log(event.target.name)
-        //setChecked(event.target.checked)
-        setChecked({ ...!checked, [event.target.name]: event.target.checked})
-    };
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    return(
-        <div>
-            <h1>Invitation</h1>
-            <div className="invitationinfo">
-                <p>You’re invited to Sander Grander’s 
-                    online birthday party 
-                    on 26.3.21 16.00-18.00!
-                    We will play Among Us.
-                </p>
-                {/* Extra checkboxit kommentoitu pois
+  return (
+    <div>
+      <h1>Invitation</h1>
+      <div className="invitationinfo">
+        <p>
+          You’re invited to Sander Grander’s online birthday party on 26.3.21
+          16.00-18.00! We will play Among Us.
+        </p>
+        {/* Extra checkboxit kommentoitu pois
                 <label>
                     <input type="checkbox"></input>
                     I will attend
@@ -66,50 +80,151 @@ const RSVP = () => {
                     I can't attend
                 </label>
                 */}
-            </div>
-            <div>
-                <FormControlLabel className={classes.checkBox}
-                    control={<Checkbox checked={checked.attending} onChange={handleChange} name="attending"/>}
-                    label="I will attend"
-                />
-            </div>
-            <div>
-                <FormControlLabel className={classes.checkBox}
-                    control={<Checkbox className={classes.checkBox} checked={checked.notattending} onChange={handleChange} name="notattending"/>}
-                    label="I can't attend"
-                />
-            </div>
-            <div className="infofield">
-                <p>Special considerations about your child:</p>
+      </div>
+      <Paper className={classes.checkBoxPaper}>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={checked.attending}
+              onChange={handleChange}
+              name="attending"
+              icon={<CheckBoxOutlineBlankIcon fontSize="large" />}
+              checkedIcon={<CheckBoxIcon fontSize="large" />}
+            />
+          }
+          label={
+            <Typography className={classes.checkBoxFont}>
+              I will attend
+            </Typography>
+          }
+        />
+      </Paper>
+      <Paper className={classes.checkBoxPaper}>
+        <FormControlLabel
+          control={
+            <Checkbox
+              className={classes.checkBox}
+              checked={checked.notattending}
+              onChange={handleChange}
+              name="notattending"
+              icon={<CheckBoxOutlineBlankIcon fontSize="large" />}
+              checkedIcon={<CheckBoxIcon fontSize="large" />}
+            />
+          }
+          label={
+            <Typography className={classes.checkBoxFont}>
+              I can't attend
+            </Typography>
+          }
+        />
+      </Paper>
+      <Typography style={{ fontSize: "2.5rem" }}>
+        Special considerations about your child:
+      </Typography>
 
-            </div>
-            <div className="gamingspecscontainer">
-                <p className="infoparagraph">Your gaming device information:</p>
-                <p className="installedparagraph">Installed</p>
-                <p className="notinstalledparagraph">Not installed</p>
-                <div><FormControlLabel className="gameinstalledcheckbox"
-                    label="Amongs Us"
-                    control={<Checkbox name="gameinstalled" onChange={handleGamingSpecChange}/>}
-                /></div>
-                <div><FormControlLabel className="discordinstalledcheckbox"
-                    label="Discord"
-                    control={<Checkbox name="discordinstalled" onChange={handleGamingSpecChange}/>}
-                /></div>
-                <div><Checkbox className="discordnotinstalledcheckbox" name="discordnotinstalled" onChange={handleGamingSpecChange}/></div>
-                <div><Checkbox className="gamenotinstalledcheckbox" name="gamenotinstalled" onChange={handleGamingSpecChange}/></div>
-            </div>
-            <Button className={classes.buttons} variant="contained" color="primary" href="/">Send</Button>
-            <div><Button className={classes.buttons} variant="outlined">More information</Button></div>
-            <div className="faqGrid">
-                <h2>Frequently asked questions</h2>
-                <p>How do I join a Party Guru party?</p>
-                <p>What kind of qualifications do Party Guru have?</p>
-                <p>What is Discord and how do I get there?</p>
-                <p>What is Minecraft?</p>
-            </div>
-
+      <div className="gamingspecscontainer">
+        <Typography style={{ fontSize: "2rem" }}>
+          Your gaming device information:
+        </Typography>
+        <Typography style={{ fontSize: "2rem" }}>Installed</Typography>
+        <Typography style={{ fontSize: "2rem" }}>Not installed</Typography>
+        <div>
+          <FormControlLabel
+            className="gameinstalledcheckbox"
+            label={
+              <Typography className={classes.checkBoxFont}>Amogus</Typography>
+            }
+            control={
+              <Checkbox
+                name="gameinstalled"
+                onChange={handleGamingSpecChange}
+                icon={<CheckBoxOutlineBlankIcon fontSize="large" />}
+                checkedIcon={<CheckBoxIcon fontSize="large" />}
+              />
+            }
+          />
         </div>
-    )
-}
+        <div>
+          <FormControlLabel
+            className="discordinstalledcheckbox"
+            label={
+              <Typography className={classes.checkBoxFont}>Discord</Typography>
+            }
+            control={
+              <Checkbox
+                name="discordinstalled"
+                onChange={handleGamingSpecChange}
+                icon={<CheckBoxOutlineBlankIcon fontSize="large" />}
+                checkedIcon={<CheckBoxIcon fontSize="large" />}
+              />
+            }
+          />
+        </div>
+        <div>
+          <Checkbox
+            className="discordnotinstalledcheckbox"
+            name="discordnotinstalled"
+            onChange={handleGamingSpecChange}
+            icon={<CheckBoxOutlineBlankIcon fontSize="large" />}
+            checkedIcon={<CheckBoxIcon fontSize="large" />}
+          />
+        </div>
+        <div>
+          <Checkbox
+            className="gamenotinstalledcheckbox"
+            name="gamenotinstalled"
+            onChange={handleGamingSpecChange}
+            icon={<CheckBoxOutlineBlankIcon fontSize="large" />}
+            checkedIcon={<CheckBoxIcon fontSize="large" />}
+          />
+        </div>
+      </div>
+      <Button
+        className={classes.buttons}
+        variant="contained"
+        color="primary"
+        href="/"
+        style={{minWidth: '80px', minHeight: '40px', fontSize:'1.2rem'}}
+      >
+        Send
+      </Button>
+        <Button className={classes.buttons} 
+        variant="contained" 
+        color="secondary"
+        style={{minWidth: '60px', minHeight: '40px', fontSize:'1.2rem'}}>
+          More information
+        </Button>
+      <Grid container direction="column">
+        <Grid item>
+          <Typography variant="h3" paragraph style={{marginTop:"5px"}}>Frequently Asked Questions</Typography>
+        </Grid>
 
-export default RSVP
+        <Grid item>
+          <Typography variant="p1">
+              How do I join a party?
+            </Typography>
+        </Grid>
+
+        <Grid item>
+          <Typography variant="p1">
+            What kind of qualifications do Party Gurus have?
+          </Typography>
+        </Grid>
+
+        <Grid item>
+          <Typography variant="p1">
+            What is Discrod and how do I install it?
+          </Typography>
+        </Grid>
+
+        <Grid item>
+          <Typography variant="p1">
+            What is Minecraft and how do I install it?
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default RSVP;
