aa04450a8db20bde9412ff4185eb22e1d6c8b357
juvalta <62938153+juvalta@users.noreply.github.com>
2021-04-09 15:37:13 +0300

    Intentional merge-error to test VSCode as mergetool (was broken)

--- partyguru_singlepage/src/screens/GuruPartyPage/GuruPartyPage.js
+++ partyguru_singlepage/src/screens/GuruPartyPage/GuruPartyPage.js
@@ -3,6 +3,8 @@ import React from 'react'
 const GuruPartyPage = () => {
     return(
         <div>Here be GuruPartyPage</div>
+        
+        <div>Test merge to test Visual Studio Code as mergetool since it didn't work before</div>
     )
 }
 export default GuruPartyPage
