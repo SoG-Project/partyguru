4d126154f7184a36c8296129169e0c400cd960dd
Joonas <joonas.vuolukka@pp.inet.fi>
2021-04-12 16:21:34 +0300

    Calendar basic functionality added

--- partyguru_singlepage/src/components/Calendar/Calendar.jsx
+++ partyguru_singlepage/src/components/Calendar/Calendar.jsx
@@ -8,12 +8,16 @@ import allLocales from "@fullcalendar/core/locales-all";
 //Hyvä demo : https://codesandbox.io/s/github/fullcalendar/fullcalendar-example-projects/tree/master/react?file=/src/DemoApp.jsx
 //Dokumentaatio: https://fullcalendar.io/docs
 const Calendar = () => {
+
+  //eventGuid is used to create IDs for events
   let eventGuid = 0;
 
+  //Used to create a new ID for the next event
   const createEventId = () => {
     return String(eventGuid++);
   };
 
+  //useState to contain all events, this should be sent to backend and recovered from there somehow
   const [events, setEvents] = React.useState([
     {
       id: createEventId(),
@@ -23,64 +27,77 @@ const Calendar = () => {
     },
   ]);
 
+  //This is called when you paint squares in the calendar and it creates an event
+  //Not necessary, calendar will probably not be "clickable" and will instead be filled through text fields etc
   const handleDateSelect = (selectInfo) => {
     let title = prompt("Please enter a new title for your event");
     let calendarApi = selectInfo.view.calendar;
-
+    let newEvent = {
+      id: createEventId(),
+      title,
+      start: selectInfo.startStr,
+      end: selectInfo.endStr,
+    }
     calendarApi.unselect(); // clear date selection
 
+    //add to calendar, then add to useState
     if (title) {
-      calendarApi.addEvent({
-        id: createEventId(),
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay,
-      });
+      calendarApi.addEvent(newEvent);
+      setEvents((events) => [...events, newEvent]);
     }
   };
 
-  const handleEventButton = (selectInfo) => {
+  //Handles clicking the Add event button on the calendar
+  //Basically does the same thing as handleDateSelect but with prompts and without touching the calendar directly
+  const handleEventButton = () => {
     console.log("HandleEventButton clicked");
     let title = prompt("Please enter a title for your event");
+      //Format is awkward, only temp solution
+      //Example valid date: '2021-04-12T10:00' -> 10am on 12.04.2021
     let startDate = prompt(
-      "Please enter event start date (in format YYYY-MM-DDTXX:XX)"
+      "Please enter event start date & time (in format YYYY-MM-DDTHH:MM)" 
     );
     let endDate = prompt(
-      "Please enter event end date (in format YYYY-MM-DDTXX:XX)"
+      "Please enter event end date & time (in format YYYY-MM-DDTXX:XX)"
     );
-    let calendarApi = selectInfo.view.calendar;
 
-    console.log(startDate, endDate);
-
+    //If all data entered and endDate is later ("greater") than startDate
     if (title && startDate && endDate && endDate > startDate) {
-      alert("Promptit täytetty oikein");
+      alert("Prompts filled correctly");
       let newEvent = {
         id: createEventId(),
         title,
         start: startDate,
         end: endDate,
       };
-      calendarApi.addEvent(newEvent);
+      //calendarApi.addEvent(newEvent);
       setEvents((events) => [...events, newEvent]);
+
     } else {
-      alert("Promptit täytetty väärin");
+      alert("Prompts filled wrong");
     }
   };
-
   return (
     <FullCalendar
       plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      //initialView: what view is open by default
       initialView="timeGridWeek"
-      initialEvents={events}
+      //events taken from useState
+      events = {events}
       height="100%"
       locale="fi"
+      //Can the calendar be painted to create events, default true, change with selectable={false}
       selectable
+      //Time when calendar starts and ends
       slotMinTime="08:00:00"
       slotMaxTime="22:00:00"
+      //Red pointer that indicates what time it is now
       nowIndicator
+      //Extra top bar that has slots for allDay events
       allDaySlot={false}
+      //Change first day to monday
       firstDay="1"
+      //What happens when the calender is "selected", aka a section is painted to create an event
       select={handleDateSelect}
       /* HeaderToolbar toimii hauskasti, jos elementtien välissä on space, tulee kalenteriin tyhjä väli, jos pilkku ne yhdistyvät
         Kokeile esim left:'prev,next today'
@@ -90,6 +107,7 @@ const Calendar = () => {
         center: "title myCustomButton",
         right: "timeGridWeek timeGridDay",
       }}
+      //For defining custom buttons, pretty simple as you can see
       customButtons={{
         myCustomButton: {
           text: "Add event",
