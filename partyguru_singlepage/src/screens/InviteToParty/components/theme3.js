0fc942fc5a2a772cdf2149ba89f65916cc843a18
huikes <alvari.leviakangas@hotmail.fi>
2021-04-14 01:30:08 +0300

    you can now swap between two themes in an unorthodox way, also added the theme images to project folder

---
+++ partyguru_singlepage/src/screens/InviteToParty/components/theme3.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+//This kind of works but I can only swap between two different themes with this
+const imagesPath = {
+    flame: "https://kjeh.fi/iKTpb",
+    space: "https://kjeh.fi/gLuYo", //can't use this yet
+    basic: "https://i.pinimg.com/originals/59/2e/81/592e812f43f66758178347430b992436.png"
+  }
+  
+  class Themeswap extends React.Component {
+    state = {
+      open: true
+    }
+    toggleImage = () => {
+      this.setState(state => ({ open: !state.open }))
+    }
+  
+    getImageName = () => this.state.open ? 'basic' : 'flame'
+  
+    render() {
+      const imageName = this.getImageName();
+      return (
+        <div>
+          <img src={imagesPath[imageName]} onClick={this.toggleImage} />
+        </div>
+      );
+    }
+  }
+  export default Themeswap;
