cc1ad4669f460caf48972126c19a5f9e58779310
huikes <alvari.leviakangas@hotmail.fi>
2021-04-20 02:04:48 +0300

    we can (sort of) do emails now, very poggers

---
+++ partyguru_singlepage/src/screens/InviteToParty/components/Form.js
@@ -0,0 +1,30 @@
+//This is the component that displays the form in which you send the email
+import React from 'react';
+import emailjs from 'emailjs-com';
+
+export default function ContactUs() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_waldej5', 'template_zmddnqe', e.target, 'user_MRLXa8iH3FjjcAzE7h6Pg')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+}
