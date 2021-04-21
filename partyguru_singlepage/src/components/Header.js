48c14d34692bc4f7d475c09a035e87c1345d4316
juvalta <62938153+juvalta@users.noreply.github.com>
2021-04-21 16:27:27 +0300

    Changed Header. AccountCircle dropdown is a test!

--- partyguru_singlepage/src/components/Header.js
+++ partyguru_singlepage/src/components/Header.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import { React, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
-import {Toolbar, AppBar} from '@material-ui/core';
+import {Toolbar, AppBar, IconButton, Menu, MenuItem} from '@material-ui/core';
 import Link from '@material-ui/core/Link';
 import {makeStyles} from '@material-ui/core/styles';
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import { withRouter } from 'react-router-dom'
+import { Theme } from '@fullcalendar/common';
 
 const useStyles = makeStyles({
     headerLink:{
         fontSize: "2rem",
         color:"white",
-    }
+    },
+    menuElement: {
+        padding: "2vh",
+    },
+    nobuttonjerk: {
+        
+    },
 })
 
-const Header = () => {
+const Header = (props) => {
+    const { history } = props
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = useState(null)
+    const open = Boolean(anchorEl)
+
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleMenuClick = (url) => {
+        history.push(url)
+        setAnchorEl(null)
+    }
     
     return (
         <AppBar position="sticky">
@@ -21,23 +46,50 @@ const Header = () => {
                 direction="row" 
                 justify="space-between">
                     <Grid item>
-                        <Link underline="none" className={classes.headerLink} href="/">Party Guru</Link>
+                        <a href="/"> <img style={{width: "10%"}} src={'https://sog.gg/wp-content/uploads/2019/08/SOG_logo_black-01.png'} /> </a>
                     </Grid>
 
-                    <Grid item>
-                        <Link underline="none" className={classes.headerLink} href="/cart">Cart</Link>
+                    <Grid item className={classes.menuElement}>
+                        <Link  underline="none" className={classes.headerLink} href="/cart">Cart</Link>
                     </Grid>
 
-                    <Grid item>
+                    <Grid item className={classes.menuElement}>
                         <Link underline="none" className={classes.headerLink} href='/gurupage/'>Guru Page</Link>
                     </Grid>
 
-                    <Grid item>
+                    <Grid item className={classes.menuElement}>
                         <Link underline="none" className={classes.headerLink} href="/login">Login</Link>
+                    </Grid>
+                    <Grid item xs={1} style={{marginTop: "0.5vh", marginLeft: "2vh"}}>
+                        <IconButton 
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleMenu}
+                        >
+                            <AccountCircle fontSize="large" />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={open}
+                            onClose={() => setAnchorEl(null)}
+                        >
+                            <MenuItem style={{width: "100%"}} onClick={() => handleMenuClick("/")}>Koti </MenuItem>
+                        </Menu>
                     </Grid>
                 </Grid>
             </Toolbar>
         </AppBar>
     )
 }
-export default Header;
+export default withRouter(Header);
