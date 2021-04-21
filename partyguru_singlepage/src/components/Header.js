0e2a833c32f01b46954d14d16949aa5d6bfc3a37
juvalta <62938153+juvalta@users.noreply.github.com>
2021-04-21 16:40:46 +0300

    Documentation to Header.js with comments

--- partyguru_singlepage/src/components/Header.js
+++ partyguru_singlepage/src/components/Header.js
@@ -21,8 +21,12 @@ const useStyles = makeStyles({
 })
 
 const Header = (props) => {
+    //history is from withRouter. withRouter gives you access to props, which in turn contains, for example,
+    //the history element. If you want to move to a different page, then it is done with history.push(url of page)
     const { history } = props
     const classes = useStyles();
+    //anchorEl defines where the pop-up menu when clicking on a menu icon should open. null means that it
+    //is not visible yet! Setting it to other than null will open the menu.
     const [anchorEl, setAnchorEl] = useState(null)
     const open = Boolean(anchorEl)
 
@@ -34,6 +38,7 @@ const Header = (props) => {
         setAnchorEl(event.currentTarget)
     }
 
+    //history.push allows us to move to a set url. setAnchorEl(null) will hide the pop-up menu.
     const handleMenuClick = (url) => {
         history.push(url)
         setAnchorEl(null)
@@ -73,7 +78,7 @@ const Header = (props) => {
                             id="menu-appbar"
                             anchorEl={anchorEl}
                             anchorOrigin={{
-                                vertical: 'top',
+                                vertical: 'bottom',
                                 horizontal: 'right',
                             }}
                             keepMounted
@@ -84,7 +89,7 @@ const Header = (props) => {
                             open={open}
                             onClose={() => setAnchorEl(null)}
                         >
-                            <MenuItem style={{width: "100%"}} onClick={() => handleMenuClick("/")}>Koti </MenuItem>
+                            <MenuItem style={{width: "100%"}} onClick={() => handleMenuClick("/login")}>Login </MenuItem>
                         </Menu>
                     </Grid>
                 </Grid>
@@ -92,4 +97,6 @@ const Header = (props) => {
         </AppBar>
     )
 }
+//withRouter allows access to the history prop, which makes us able to move
+//to the homepage with history.push.
 export default withRouter(Header);
