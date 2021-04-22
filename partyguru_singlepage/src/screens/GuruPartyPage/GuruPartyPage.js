336cd0a1e7443c36eddeef875a7427bd41fe36c7
juvalta <62938153+juvalta@users.noreply.github.com>
2021-04-22 17:56:00 +0300

    GururPartyPage added updated texbox/checkbox elements from CrParPage

--- partyguru_singlepage/src/screens/GuruPartyPage/GuruPartyPage.js
+++ partyguru_singlepage/src/screens/GuruPartyPage/GuruPartyPage.js
@@ -1,4 +1,4 @@
-import {React, useState, useEffect} from "react";
+import { React, useState, useEffect } from "react";
 import Axios from "axios";
 
 import {
@@ -12,9 +12,11 @@ import {
   Box,
 } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
-import {shadows} from '@material-ui/system'
-import PartyHeroInfo from './components/PartyHeroInfo'
-import Attendees from './components/Attendees'
+import { shadows } from "@material-ui/system";
+import PartyHeroInfo from "./components/PartyHeroInfo";
+import Attendees from "./components/Attendees";
+import CheckBoxes from "../CreatePartyPage/components/CheckBoxes"
+import GameInfo from "../CreatePartyPage/components/GameInfo"
 import axios from "axios";
 import { isPropsEqual } from "@fullcalendar/react";
 
@@ -57,50 +59,47 @@ const useStyles = makeStyles((theme) => ({
     height: "50%",
   },
   test: {
-    height: "100%"
+    height: "100%",
   },
 }));
 
 const GuruPartyPage = (props) => {
-  const partyID="605f8bcd8dfd970aa770584b";
+  const partyID = "605f8bcd8dfd970aa770584b";
   const classes = useStyles();
-  const [party, setParty]=useState({});
-  const [partyheroinfo, changePartyHeroInfo] = useState("Display information about the Party hero here")
+  const [party, setParty] = useState({});
+  const [partyheroinfo, changePartyHeroInfo] = useState(
+    "Display information about the Party hero here"
+  );
 
   useEffect(() => {
     //getData gets partypack in question.
-    getData()
+    getData();
     //If you console.log here, it will not display the response gotten from the server since further code is being executed
     //already since code is async. That means console log here is pointless. Try console.log in .then() function in getData()
     //console.log(description, " is the description")
-  }, [])
+  }, []);
 
-//getData gets the partypack in question from the server. axios.get() is an asynchronous function, so anything
-//not in the .then() {} brackets will be executed before we get a response from the server. How far in the code we
-//get depends on the execution time of the get function.
-const getData = () => {
+  //getData gets the partypack in question from the server. axios.get() is an asynchronous function, so anything
+  //not in the .then() {} brackets will be executed before we get a response from the server. How far in the code we
+  //get depends on the execution time of the get function.
+  const getData = () => {
     //axios gets the partypack
-    axios.get(`/api/parties/${partyID}`).then(response => {
-
-    setParty(response.data);
-    changePartyHeroInfo(response.data.partyheroinfo);
-    console.log(response.data.partyheroinfo)
-
-})
-
-}
-
+    axios.get(`/api/parties/${partyID}`).then((response) => {
+      setParty(response.data);
+      changePartyHeroInfo(response.data.partyheroinfo);
+      console.log(response.data.partyheroinfo);
+    });
+  };
 
   const handlePartyHeroInfoChange = (event) => {
-    let info = event.target.value
-    changePartyHeroInfo(info)
-  }
+    let info = event.target.value;
+    changePartyHeroInfo(info);
+  };
 
   return (
     <div className="maindiv">
-      
       <div className="partyguruinfobox">
-        <Grid container direction="column" alignItems="center" >
+        <Grid container direction="column" alignItems="center">
           <Typography variant="h2">Party Guru</Typography>
           <Typography variant="subtitle" gutterBottom>
             Our Party Gurus host the most awesome parties for partiers of any
@@ -115,58 +114,60 @@ const getData = () => {
           </Typography>
         </Grid>
       </div>
+      <Attendees />
       <Grid
-          container
-          justify="space-around"
-          direction="row"
-          alignItems="stretch"
-          style={{ marginTop: "2rem", marginBottom: "1rem" }}
-        >
-          <Grid item xs={5}>
-      <Paper elevation={4} style={{ height: "100%" }}>
-              <Grid
-                container
-                item
-                direction="column"
-                style={{
-                  padding: "2rem",
-                }}
-              >
-                <Grid item xs={12}>
-                  <Typography gutterBottom paragraph variant="h4">
-                    Special considerations about your child
-                  </Typography>
-                </Grid>
-
-                <Grid item xs={12}>
-                  <TextField
-                    color="primary"
-                    multiline
-                    fullWidth
-                    rows={4}
-                    rowsMax={5}
-                    id="namefield"
-                    label={
-                      <Typography style={{ fontSize: "2rem" }}>
-                        Enter info here
-                      </Typography>
-                    }
-                    variant="outlined"
-                    inputProps={{
-                      maxLength: 300,
-                      style: { fontSize: "2rem", lineHeight: "150%" },
-                    }}
-                  />
-                </Grid>
+        container
+        justify="space-around"
+        direction="row"
+        alignItems="stretch"
+        style={{ marginTop: "2rem", marginBottom: "1rem" }}
+      >
+        <Grid item xs={5}>
+          <Paper elevation={4} style={{ height: "100%" }}>
+            <Grid
+              container
+              item
+              direction="column"
+              style={{
+                padding: "2rem",
+              }}
+            >
+              <Grid item xs={12}>
+                <Typography gutterBottom paragraph variant="h4">
+                  Special considerations about your child
+                </Typography>
               </Grid>
-            </Paper>
+
+              <Grid item xs={12}>
+                <TextField
+                  color="primary"
+                  multiline
+                  fullWidth
+                  rows={4}
+                  rowsMax={5}
+                  id="namefield"
+                  label={
+                    <Typography style={{ fontSize: "2rem" }}>
+                      Enter info here
+                    </Typography>
+                  }
+                  variant="outlined"
+                  inputProps={{
+                    maxLength: 300,
+                    style: { fontSize: "2rem", lineHeight: "150%" },
+                  }}
+                />
+              </Grid>
             </Grid>
-            </Grid>
-      <Attendees/>
+          </Paper>
+        </Grid>
+        <Grid item xs={5}>
+          <CheckBoxes />
+        </Grid>
+      </Grid>
+      
       <div>
-        
-        <PartyHeroInfo description={partyheroinfo}/>
-        
+        <PartyHeroInfo description={partyheroinfo} />
       </div>
     </div>
   );
--- partyguru_singlepage/src/screens/LandingPage/LandingPage.js
+++ partyguru_singlepage/src/screens/LandingPage/LandingPage.js
@@ -1,19 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import Product from "../../components/Product";
-import ContactInfoFields from "../../components/ContactInfoFields";
-import AttendeeNumberSelector from "../../components/AttendeeNumberSelector";
-import CostCalculator from "../../components/CostCalculator";
-import Calendar from "../../components/Calendar/Calendar";
 
-import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import { Link } from "react-router-dom";
 import axios from "axios";
-import SchedulerTest from "../../components/Scheduler/SchedulerTest";
-import Divider from "@material-ui/core/Divider";
-import EditableScheduler from "../../components/Scheduler/EditableScheduler";
 import { Typography } from "@material-ui/core";
 //splash sivu
 //map funktio looppaa datan läpi ja tekee niistä kortteja
