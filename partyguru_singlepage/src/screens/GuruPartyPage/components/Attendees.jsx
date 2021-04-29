72a32886299f3c9292a00499ecf2a65273cad242
huikes <alvari.leviakangas@hotmail.fi>
2021-04-29 12:22:50 +0300

    fixed the issue of fetching only one attendee from backend

--- partyguru_singlepage/src/screens/GuruPartyPage/components/Attendees.jsx
+++ partyguru_singlepage/src/screens/GuruPartyPage/components/Attendees.jsx
@@ -40,7 +40,7 @@ const Attendees = (props) => {
             <Grid container direction="row">
               <Grid item xs={10}>
                 <Typography className={classes.attendeeText}>
-                  {attendees[attendees.length - 1].name}
+                  {attendees[i].name}
                 </Typography>
               </Grid>
               <Grid item xs={2}>
