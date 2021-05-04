3a2f2fe580db090c03d59ab99ab522573bfdd76a
juvalta <62938153+juvalta@users.noreply.github.com>
2021-05-04 16:48:09 +0300

    Schedule prettier on PartyPage

--- partyguru_singlepage/src/screens/PartyPage/PartyPage.js
+++ partyguru_singlepage/src/screens/PartyPage/PartyPage.js
@@ -129,24 +129,28 @@ const PartyPage = () => {
           <Attendees attendeesArray={attendeeInfo} />
         </Grid>
       </Grid>
-      <Typography variant="h5">Schedule</Typography>
+      <Typography variant="h3">Schedule</Typography>
       <Grid
           container
-          justify="space-around"
+          justify="flex-start"
           direction="row"
-          style={{ backgroundColor: "orange", marginBottom: "1rem" }}
+          style={{ marginBottom: "6rem", marginTop: "3rem" }}
       >
         {partyPackage.scheduleitems &&
         partyPackage.scheduleitems.map((item) => (
-            <Typography key={item} style={{ fontSize: "1.5rem" }}>
-              {item}
-            </Typography>
+            <Grid item xs={2}>
+              <Paper style={{backgroundColor: "lightgray", paddingLeft:"4vh", paddingRight:"4vh", paddingBottom:"2vh", paddingTop: "2vh", margin: "1vh"}}>
+                <Typography key={item} style={{ fontSize: "1.5rem" }}>
+                  {item}
+                </Typography>
+              </Paper>
+            </Grid>
         ))}
       </Grid>
 
       <Grid container direction="row">
       {contactInfo && (
-      <Grid container item xs={6} direction="column" spacing={1}>
+      <Grid container item xs={6} direction="column" spacing={1} justify="flex-start">
           <Grid item>
             <Typography variant="h3" style={{ marginTop: "2rem" }} >
               Client Information
